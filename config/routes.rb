23ea89ea3bd5b167179bea617df5b043513bce02
Carl Tablante <carl.q.tablante@gmail.com>
2014-05-25 17:43:37 +0800

    template for sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 TimeManager::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
   get "site/index"
 
-  root 'site#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
